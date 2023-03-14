5dfaea1d239eb4781be2eb7a613eb4a725350d04
mochicastle <michelle@mochicastle.com>
2023-03-14 17:01:47 -0400

    added error handling for null Yelp results

--- client/src/services/wildcardRestaurants.js
+++ client/src/services/wildcardRestaurants.js
@@ -1,19 +1,22 @@
 const wildcardRestaurants = async (wildcardData) => {
-    let wildcardPick
+  let wildcardPick;
 
-    try {
-        const response = await fetch(`/api/v1/wildcard?categories=${wildcardData.categories}&latitude=${wildcardData.latitude}&longitude=${wildcardData.longitude}&radius=${wildcardData.radius}&price=${wildcardData.price}&term=${wildcardData.term}&open_now=${wildcardData.open_now}`)
-        if (!response.ok) {
-            const errorMessage = `${response.status} (${response.statusText})`
-            const error = new Error(errorMessage)
-            throw(error);
-          }
-          const wildcardRestaurant = await response.json()
-          wildcardPick = wildcardRestaurant
-          return wildcardPick
-    } catch (error) {
-        console.error(`Error in fetch: ${error.message}`)
+  try {
+    const response = await fetch(
+      `/api/v1/wildcard?categories=${wildcardData.categories}&latitude=${wildcardData.latitude}&longitude=${wildcardData.longitude}&radius=${wildcardData.radius}&price=${wildcardData.price}&term=${wildcardData.term}&open_now=${wildcardData.open_now}`
+    );
+    if (!response.ok) {
+      const errorMessage = `${response.status} (${response.statusText})`;
+      const error = new Error(errorMessage);
+      throw error;
     }
-}
+    const wildcardRestaurant = await response.json();
+    wildcardPick = wildcardRestaurant;
+    // console.log("wildcardPick: ", wildcardPick);
+    return wildcardPick;
+  } catch (error) {
+    console.error(`Error in fetch: ${error.message}`);
+  }
+};
 
-export default wildcardRestaurants
+export default wildcardRestaurants;
--- server/src/routes/api/v1/wildcardRouter.js
+++ server/src/routes/api/v1/wildcardRouter.js
@@ -1,35 +1,32 @@
-import express from "express"
+import express from "express";
 
-import YelpClient from "../../../apiClient/YelpClient.js"
-import radiusConversion from "../../../services/radiusConversion.js"
-import pickWildcard from "../../../services/pickWildcard.js"
+import YelpClient from "../../../apiClient/YelpClient.js";
+import radiusConversion from "../../../services/radiusConversion.js";
+import pickWildcard from "../../../services/pickWildcard.js";
 
-const wildcardRouter = new express.Router()
+const wildcardRouter = new express.Router();
 
 wildcardRouter.get("/", async (req, res) => {
+  const wildcardData = req.query;
+  const convertedRadius = radiusConversion(wildcardData.radius);
+  let wildcard = {};
+  wildcard = {
+    ...wildcardData,
+    radius: convertedRadius,
+  };
 
-    const wildcardData = req.query
-    const convertedRadius = radiusConversion(wildcardData.radius)
-    let wildcard = {}
-    wildcard = {
-        ...wildcardData,
-        radius: convertedRadius
-    }
+  try {
+    const yelpClient = new YelpClient();
+    const restaurantsResponse = await yelpClient.search(wildcard);
 
-    try {
-        const yelpClient = new YelpClient
-        const restaurantsResponse = await yelpClient.search(wildcard)
+    const restaurantsData = restaurantsResponse.jsonBody;
+    const wildcardPick = pickWildcard(restaurantsData.businesses);
+    // console.log(wildcardPick);
+    return res.set({ "Content-Type": "application/json" }).status(200).json(wildcardPick);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ errors: error });
+  }
+});
 
-        const restaurantsData = restaurantsResponse.jsonBody
-        const wildcardPick = pickWildcard(restaurantsData.businesses)
-        return res
-            .set({ "Content-Type": "application/json" })
-            .status(200)
-            .json(wildcardPick)
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({ errors: error })
-    }
-})
-
-export default wildcardRouter
+export default wildcardRouter;
